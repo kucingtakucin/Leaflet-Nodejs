64b54746ce1008b8abe7c8d8678d18ebb78a0d93
Adam Arthur Faizal <adam.faizal.af6@student.uns.ac.id>
2021-06-26 21:38:33 +0700

    initial commit

---
+++ examples/001-Leaflet-BaseLayers/app.js
@@ -0,0 +1,33 @@
+// Initialize leaflet.js
+let L = require('leaflet');
+let $ = require('jquery');
+
+// Initialize the map
+let map = L.map('map', {
+	center: [-7.60773230, 110.98023906],
+	zoom: 13,
+	layers: [
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			id: 'basemap',
+			attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+		}),
+		L.marker().setLatLng([-7.60773230, 110.98023906])
+		.bindPopup("<b>Hello world!</b><br>I am a popup.")
+		.openPopup()
+	]
+});
+map.on('click', function (event) {
+	L.popup().setLatLng(event.latlng)
+		.setContent("You clicked the map at " + event.latlng.toString())
+		.openOn(map);
+})
+
+
+fetch(`./map-kab-kra.geojson`)
+	.then(response => response.json())
+	.then(response => {
+		console.log(response)
+	})
+	.catch(error => {
+		console.error(error)
+	})
